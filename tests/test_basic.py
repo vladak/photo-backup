8712366bfb8878206498cd92ff43623f0684c41d
Vladimir Kotal <vlada@devnull.cz>
2019-09-06 22:33:28 +0200

    enable flake8 for tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -2,13 +2,18 @@ import exiftool
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 from photo_backup.exif import check_keywords
+
+sys.path.insert(0,
+                os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 
+
 def test_check_keywords():
     with exiftool.ExifTool() as et:
-        assert check_keywords(et, os.path.join(DIR_PATH, "testfile.jpg"), ["selected"])
-        assert not check_keywords(et, os.path.join(DIR_PATH, "testfile.jpg"), ["foo"])
+        assert check_keywords(et, os.path.join(DIR_PATH, "testfile.jpg"),
+                              ["selected"])
+        assert not check_keywords(et, os.path.join(DIR_PATH, "testfile.jpg"),
+                                  ["foo"])
