2ecce29d09eef1e54aefe1ef3ee53357211beb65
Vladimir Kotal <vlada@devnull.cz>
2019-09-07 22:48:48 +0200

    rename

--- tests/test_backup.py
+++ tests/test_backup.py
@@ -13,7 +13,7 @@ sys.path.insert(0,
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 
 
-def test_handling():
+def test_backup():
     with tempfile.TemporaryDirectory() as srcdir:
         # Create simple source directory structure.
         subdir_name = "subdir"
