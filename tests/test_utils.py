55b07dee1e499fc46c7b0b229f66814f0c19f5b1
Vladimir Kotal <vlada@devnull.cz>
2021-09-26 22:44:38 +0200

    test sys.exit(1) from check_dir()

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,9 @@
 import os
 import sys
 
-from photo_backup.utils import check_suffix
+import pytest
+
+from photo_backup.utils import check_suffix, check_dir
 
 sys.path.insert(0,
                 os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
@@ -18,3 +20,10 @@ def test_suffix_positive():
 
 def test_suffix_negative():
     assert not check_suffix("foo.bar", ["x", "y", "foo", "z"])
+
+
+def test_check_dir_negative():
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        check_dir("/nonexistent")
+    assert pytest_wrapped_e.type == SystemExit
+    assert pytest_wrapped_e.value.code == 1
